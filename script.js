df804eccaa4deb406ac0549e4016e4c22919f854
Va5ud3va1 <va5ud3va1@outlook.com>
2024-05-10 20:54:33 +0530

    hello

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
